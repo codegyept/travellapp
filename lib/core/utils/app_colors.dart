f4847c75c3378d0d211bdc976240b1ad2e30265f
Osama Abd Al Malik <osamaaabdalmalik@gmail.com>
2024-03-10 19:58:15 +0300

    add some paths to `app_assets.dart` and add some colors to `app_colors.dart`

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -6,6 +6,8 @@ class AppColors
   static const textAndBackgroundColorButton = Color(0xff00A6E7);
   static const backgroundColorExpansionAndText = Color(0xFF263238);
   static const white = Colors.white;
+  static const backgroundWhite = Color(0xFFFAFAFA);
+  static const backgroundAvatarPayment = Color(0xFFAE9F60);
   static const black = Colors.black;
   static const grey = Colors.grey;
 }
