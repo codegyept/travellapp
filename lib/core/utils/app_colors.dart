25b3e82c517ae73b390818d4f9e7be5cdaacd6da
Osama Abd Al Malik <osamaaabdalmalik@gmail.com>
2024-03-10 11:17:55 +0300

    add some colors on `app_colors.dart`

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -4,4 +4,7 @@ import 'package:flutter/material.dart';
 class AppColors
 {
   static const textAndBackgroundColorButton = Color(0xff00A6E7);
+  static const backgroundColorExpansionAndText = Color(0xFF263238);
+  static const white = Colors.white;
+  static const grey = Colors.grey;
 }
