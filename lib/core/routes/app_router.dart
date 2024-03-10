9fab04723b5fbb536cc1a491f9772f01bb3d8deb
Osama Abd Al Malik <osamaaabdalmalik@gmail.com>
2024-03-10 12:49:36 +0300

    add screen routes in `app_router.dart`

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,4 +1,8 @@
 import 'package:go_router/go_router.dart';
+import 'package:rehlatyuae/features/home_screen/presentaion/views/about_us_screen.dart';
+import 'package:rehlatyuae/features/home_screen/presentaion/views/faq_screen.dart';
+import 'package:rehlatyuae/features/home_screen/presentaion/views/privacy_policy_screen.dart';
+import 'package:rehlatyuae/features/home_screen/presentaion/views/terms_conditions_screen.dart';
 
 import '../../features/splash_screen/presentaion/views/splash_screen.dart';
 
@@ -7,7 +11,23 @@ abstract class AppRouter {
     routes: [
       GoRoute(
         path: "/",
-        builder:(context, state) => const SplashScreen(),
+        builder: (context, state) => const SplashScreen(),
+      ),
+      GoRoute(
+        path: "/aboutUsScreen",
+        builder: (context, state) => const AboutUsScreen(),
+      ),
+      GoRoute(
+        path: "/termsConditionsScreen",
+        builder: (context, state) => const TermsConditionsScreen(),
+      ),
+      GoRoute(
+        path: "/privacyPolicyScreen",
+        builder: (context, state) => const PrivacyPolicyScreen(),
+      ),
+      GoRoute(
+        path: "/faqsScreen",
+        builder: (context, state) => const FAQsScreen(),
       ),
     ],
   );
