bbc09ca1cc70aa7dcff5fabaa1786a8c26783bb5
Osama Abd Al Malik <osamaaabdalmalik@gmail.com>
2024-03-12 02:41:13 +0300

    add profile screen with some assets

--- lib/core/utils/app_assets.dart
+++ lib/core/utils/app_assets.dart
@@ -3,4 +3,5 @@ class AppAssets {
   static const String logoMastercard = 'assets/svgs/logo_mastercard.svg';
   static const String logoVisa = 'assets/svgs/logo_visa.svg';
   static const String rectangle = 'assets/images/rectangle.png';
+  static const String profile = 'assets/images/profile.png';
 }
