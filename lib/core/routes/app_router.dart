b31a73f68880525253e7b1e46077fc5a3fc2344c
Osama Abd Al Malik <osamaaabdalmalik@gmail.com>
2024-03-12 05:15:47 +0300

    add blog screen and update `app_router`

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,10 +1,13 @@
 import 'package:go_router/go_router.dart';
 import 'package:rehlatyuae/features/home_screen/presentaion/views/about_us_screen.dart';
+import 'package:rehlatyuae/features/home_screen/presentaion/views/blog_screen.dart';
 import 'package:rehlatyuae/features/home_screen/presentaion/views/faq_screen.dart';
 import 'package:rehlatyuae/features/home_screen/presentaion/views/payment_details_screen.dart';
 import 'package:rehlatyuae/features/home_screen/presentaion/views/payment_options_screen.dart';
 import 'package:rehlatyuae/features/home_screen/presentaion/views/privacy_policy_screen.dart';
+import 'package:rehlatyuae/features/home_screen/presentaion/views/profile_screen.dart';
 import 'package:rehlatyuae/features/home_screen/presentaion/views/terms_conditions_screen.dart';
+import 'package:rehlatyuae/features/home_screen/presentaion/views/travel_details_screen.dart';
 import 'package:rehlatyuae/features/splash_screen/presentation/views/splash_screen.dart';
 
 abstract class AppRouter {
@@ -37,6 +40,18 @@ abstract class AppRouter {
       GoRoute(
         path: "/paymentDetailsScreen",
         builder: (context, state) => const PaymentDetailsScreen(),
+      ),
+      GoRoute(
+        path: "/profileScreen",
+        builder: (context, state) => const ProfileScreen(),
+      ),
+      GoRoute(
+        path: "/travelDetailsScreen",
+        builder: (context, state) => const TravelDetailsScreen(),
+      ),
+      GoRoute(
+        path: "/blogScreen",
+        builder: (context, state) => const BlogScreen(),
       ),
     ],
   );
