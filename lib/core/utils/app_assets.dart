e9274341f78232f2983929e018775b789e1cf568
Osama Abd Al Malik <osamaaabdalmalik@gmail.com>
2024-03-15 23:31:16 +0300

    add assets for auth

--- lib/core/utils/app_assets.dart
+++ lib/core/utils/app_assets.dart
@@ -1,8 +1,30 @@
 class AppAssets {
+  /// Svgs
   static const String arrowUp = 'assets/svgs/arrow_up.svg';
   static const String logoMastercard = 'assets/svgs/logo_mastercard.svg';
   static const String logoVisa = 'assets/svgs/logo_visa.svg';
+  static const String homeIcon = 'assets/svgs/home_icon.svg';
+  static const String searchIcon = 'assets/svgs/search_icon.svg';
+  static const String exploreIcon = 'assets/svgs/explore_icon.svg';
+  static const String bookingIcon = 'assets/svgs/booking_icon.svg';
+  static const String accountIcon = 'assets/svgs/account_icon.svg';
+  static const String blueLogo = 'assets/svgs/blue_logo.svg';
+  static const String credits = 'assets/svgs/credits.svg';
+  static const String currency = 'assets/svgs/currency.svg';
+  static const String language = 'assets/svgs/language.svg';
+  static const String notifications = 'assets/svgs/notifications.svg';
+  static const String myBooking = 'assets/svgs/my_booking.svg';
+  static const String sendMessage = 'assets/svgs/send_message.svg';
+  static const String privacyPolicy = 'assets/svgs/privacy_policy.svg';
+  static const String terms = 'assets/svgs/terms.svg';
+  static const String updateApp = 'assets/svgs/update_app.svg';
+  static const String aboutApp = 'assets/svgs/about_app.svg';
+  static const String logout = 'assets/svgs/logout.svg';
+
+  /// Images
   static const String rectangle = 'assets/images/rectangle.png';
   static const String profile = 'assets/images/profile.png';
   static const String travel = 'assets/images/travel.png';
+  static const String loginBackground = 'assets/images/login.png';
+  static const String registerBackground = 'assets/images/register.png';
 }
