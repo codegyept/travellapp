a5a5139b70587cc11c39aee3be8bebc9baa90143
Osama Abd Al Malik <osamaaabdalmalik@gmail.com>
2024-03-22 10:30:40 +0300

    add `client_model.dart` to profile feature

---
+++ lib/features/profile/data/models/client_model.freezed.dart
@@ -0,0 +1,250 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'client_model.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+Client _$ClientFromJson(Map<String, dynamic> json) {
+  return _Client.fromJson(json);
+}
+
+/// @nodoc
+mixin _$Client {
+  int get id => throw _privateConstructorUsedError;
+
+  String get name => throw _privateConstructorUsedError;
+
+  String get email => throw _privateConstructorUsedError;
+
+  String get phone => throw _privateConstructorUsedError;
+
+  String get address => throw _privateConstructorUsedError;
+
+  @JsonKey(name: "image_path")
+  String get imagePath => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $ClientCopyWith<Client> get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $ClientCopyWith<$Res> {
+  factory $ClientCopyWith(Client value, $Res Function(Client) then) = _$ClientCopyWithImpl<$Res, Client>;
+
+  @useResult
+  $Res call(
+      {int id, String name, String email, String phone, String address, @JsonKey(name: "image_path") String imagePath});
+}
+
+/// @nodoc
+class _$ClientCopyWithImpl<$Res, $Val extends Client> implements $ClientCopyWith<$Res> {
+  _$ClientCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? name = null,
+    Object? email = null,
+    Object? phone = null,
+    Object? address = null,
+    Object? imagePath = null,
+  }) {
+    return _then(_value.copyWith(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int,
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      email: null == email
+          ? _value.email
+          : email // ignore: cast_nullable_to_non_nullable
+              as String,
+      phone: null == phone
+          ? _value.phone
+          : phone // ignore: cast_nullable_to_non_nullable
+              as String,
+      address: null == address
+          ? _value.address
+          : address // ignore: cast_nullable_to_non_nullable
+              as String,
+      imagePath: null == imagePath
+          ? _value.imagePath
+          : imagePath // ignore: cast_nullable_to_non_nullable
+              as String,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$ClientImplCopyWith<$Res> implements $ClientCopyWith<$Res> {
+  factory _$$ClientImplCopyWith(_$ClientImpl value, $Res Function(_$ClientImpl) then) =
+      __$$ClientImplCopyWithImpl<$Res>;
+
+  @override
+  @useResult
+  $Res call(
+      {int id, String name, String email, String phone, String address, @JsonKey(name: "image_path") String imagePath});
+}
+
+/// @nodoc
+class __$$ClientImplCopyWithImpl<$Res> extends _$ClientCopyWithImpl<$Res, _$ClientImpl>
+    implements _$$ClientImplCopyWith<$Res> {
+  __$$ClientImplCopyWithImpl(_$ClientImpl _value, $Res Function(_$ClientImpl) _then) : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? id = null,
+    Object? name = null,
+    Object? email = null,
+    Object? phone = null,
+    Object? address = null,
+    Object? imagePath = null,
+  }) {
+    return _then(_$ClientImpl(
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as int,
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      email: null == email
+          ? _value.email
+          : email // ignore: cast_nullable_to_non_nullable
+              as String,
+      phone: null == phone
+          ? _value.phone
+          : phone // ignore: cast_nullable_to_non_nullable
+              as String,
+      address: null == address
+          ? _value.address
+          : address // ignore: cast_nullable_to_non_nullable
+              as String,
+      imagePath: null == imagePath
+          ? _value.imagePath
+          : imagePath // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$ClientImpl implements _Client {
+  const _$ClientImpl(
+      {required this.id,
+      required this.name,
+      required this.email,
+      required this.phone,
+      required this.address,
+      @JsonKey(name: "image_path") required this.imagePath});
+
+  factory _$ClientImpl.fromJson(Map<String, dynamic> json) => _$$ClientImplFromJson(json);
+
+  @override
+  final int id;
+  @override
+  final String name;
+  @override
+  final String email;
+  @override
+  final String phone;
+  @override
+  final String address;
+  @override
+  @JsonKey(name: "image_path")
+  final String imagePath;
+
+  @override
+  String toString() {
+    return 'Client(id: $id, name: $name, email: $email, phone: $phone, address: $address, imagePath: $imagePath)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$ClientImpl &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.name, name) || other.name == name) &&
+            (identical(other.email, email) || other.email == email) &&
+            (identical(other.phone, phone) || other.phone == phone) &&
+            (identical(other.address, address) || other.address == address) &&
+            (identical(other.imagePath, imagePath) || other.imagePath == imagePath));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(runtimeType, id, name, email, phone, address, imagePath);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$ClientImplCopyWith<_$ClientImpl> get copyWith => __$$ClientImplCopyWithImpl<_$ClientImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$ClientImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _Client implements Client {
+  const factory _Client(
+      {required final int id,
+      required final String name,
+      required final String email,
+      required final String phone,
+      required final String address,
+      @JsonKey(name: "image_path") required final String imagePath}) = _$ClientImpl;
+
+  factory _Client.fromJson(Map<String, dynamic> json) = _$ClientImpl.fromJson;
+
+  @override
+  int get id;
+
+  @override
+  String get name;
+
+  @override
+  String get email;
+
+  @override
+  String get phone;
+
+  @override
+  String get address;
+
+  @override
+  @JsonKey(name: "image_path")
+  String get imagePath;
+
+  @override
+  @JsonKey(ignore: true)
+  _$$ClientImplCopyWith<_$ClientImpl> get copyWith => throw _privateConstructorUsedError;
+}
